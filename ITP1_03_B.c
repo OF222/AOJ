6489c5c4710608e2b293d8162c057d2e578f0544
OF222 <0214osuke@gmail.com>
2020-07-17 19:08:33 +0900

    課題の作成

--- ITP1_03_B.c
+++ ITP1_03_B.c
@@ -1,1 +1,19 @@
-﻿
+﻿//0が入力されたら終了するプログラム
+#include<stdio.h>
+int main(void) {
+	int input[10000] = { 0 };
+	int i = 0;
+
+	scanf("%d", &input[i]);
+	i++;
+	while (input[i-1]!=0) {
+		scanf("%d", &input[i]);
+		i++;
+	}
+	i = 0;
+	while (input[i] != 0) {
+		printf("Case %d: %d\n", i, input[i]);
+		i++;
+	}
+	return 0;
+}
