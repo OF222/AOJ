14c1e3c138a8aa5f62219e7fcc197c82c6e202c3
OF222 <0214osuke@gmail.com>
2020-07-26 02:52:38 +0900

    課題の作成

--- ITP1_04_A.c
+++ ITP1_04_A.c
@@ -1,1 +1,10 @@
-﻿
+﻿//割り算の結果を表示するプログラム
+#include<stdio.h>
+int main(void) {
+	int a, b;  //割り算に用いる数
+
+	scanf("%d %d", &a, &b);  //数の読み込み
+	printf("%d %d %f", a / b, a % b, (double)a/b);  //割り算（整数）、余り（整数）、割り算（浮動小数点数）
+
+	return 0;
+}
