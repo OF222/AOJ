e83ad26b404e4a4c671c46b2fb55ee49046e4b7c
OF222 <0214osuke@gmail.com>
2020-07-27 00:57:42 +0900

    課題の作成

--- ITP1_06_A.c
+++ ITP1_06_A.c
@@ -1,1 +1,20 @@
-﻿
+﻿//与えられた数を逆順に表示するプログラム
+#include<stdio.h>
+
+int main(void) {
+	int n;
+	int sequence[100];
+	int i;
+
+	scanf("%d", &n);  //入力する数n個の指定
+	for (i = 0;i < n;i++) {
+		scanf("%d", &sequence[i]);  //数を入力する
+	}
+	//数を逆順にして表示する
+	for (i = n -1;i > 0;i--) {
+		printf("%d ", sequence[i]);
+	} 
+	printf("%d\n", sequence[i]);
+
+	return 0;
+}
--- ITP1_06_C.c
+++ ITP1_06_C.c
@@ -1,1 +1,33 @@
-﻿
+﻿#include<stdio.h>
+
+int main(void) {
+	int n, b, f, r, v;
+	int i, j, k;
+	int resident[4][3][10] = { {0} };
+	
+	scnaf("%d", &n);
+	for (i = 0;i < n;i++) {
+		scanf("%d %d %d %d", &b, &f, &r, &v);
+		resident[b - 1][f - 1][r - 1] = resident[b - 1][f - 1][r - 1] + v;
+	}
+	
+	for (j = 0;j < 3;j++) {
+		for (k = 0;k < 10;k++) {
+			printf("%d", resident[0][j][k]);
+		}
+		printf("\n");
+	}
+	for (i = 1;i < 4;i++) {
+		for (k = 0;k < 20;k++) {
+			printf("#");
+		}
+		printf("\n");
+		for (j = 0;j < 3;j++) {
+			for (k = 0;k < 10;k++) {
+				printf("%d", resident[i][j][k]);
+			}
+			printf("\n");
+		}
+	}
+	return 0;
+}
