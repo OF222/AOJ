1a2c63f94945cb3b63adc34c1a9b0d6bdb2b15c3
OF222 <0214osuke@gmail.com>
2020-07-26 14:40:11 +0900

    課題の作成

--- ITP1_05_A.c
+++ ITP1_05_A.c
@@ -1,1 +1,20 @@
-﻿
+﻿//長方形を描写するプログラム
+#include<stdio.h>
+int main(void) {
+	int i, j;
+	int H, W;
+
+	scanf("%d %d", &H, &W);
+	while ((H != 0) && (W != 0)) {
+		for (i = 0;i < H;i++) {
+			for (j = 0;j < W;j++) {
+				printf("#");
+			}
+			printf("\n");
+		}
+		printf("\n");
+		scanf("%d %d", &H, &W);
+	}
+
+	return 0;
+}
--- ITP1_05_B.c
+++ ITP1_05_B.c
@@ -1,1 +1,27 @@
-﻿
+﻿//長方形を描写するプログラム
+#include<stdio.h>
+int main(void) {
+	int i, j;
+	int H, W;  //縦、横の入力に用いる変数
+
+	scanf("%d %d", &H, &W);  //縦、横の入力
+	while ((H != 0) && (W != 0)) {  //H,Wともに0まで繰り返す
+		for (i = 0;i < H;i++) {
+			for (j = 0;((i == 0) || (i == H-1))&&(j < W);j++) {  //最初と最後の行の描写
+				printf("#");
+			}
+			if ((i != 0) && (i != H - 1)) {  //最初と最後以外の行の描写
+				printf("#");
+				for (j = 1;j < W - 1;j++) {
+					printf(".");
+				}
+				printf("#");
+			}
+			printf("\n");
+		}
+		printf("\n");
+		scanf("%d %d", &H, &W);  //縦、横の入力
+	}
+
+	return 0;
+}
