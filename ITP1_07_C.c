0994dcc594f0b19a338ec81cc331342b3164f8af
OF222 <0214osuke@gmail.com>
2020-07-30 21:02:13 +0900

    課題の作成

--- ITP1_07_C.c
+++ ITP1_07_C.c
@@ -2,15 +2,17 @@
 #include<stdio.h>
 
 int main(void) {
-	int table[100][100] = { {0} };
+	int table[101][101] = { {0} };  //表の入力用の配列の宣言
 	int m, n, i, j;
 
-	scanf("%d %d", &m, &n);
+	scanf("%d %d", &m, &n);  //行、列数の指定
+	//値の入力
 	for (i = 0;i < m;i++) {
 		for (j = 0;j < n;j++) {
 			scanf("%d", &table[i][j]);
 		}
 	}
+	//和の計算
 	for (j = 0;j < n;j++) {
 		for (i = 0;i < m;i++) {
 			table[m][j] = table[m][j] + table[i][j];
@@ -22,6 +24,7 @@ int main(void) {
 		}
 	}
 
+	//表の表示
 	for (i = 0;i < m + 1;i++) {
 		for (j = 0;j < n;j++) {
 			printf("%d ", table[i][j]);
--- ITP1_07_D.c
+++ ITP1_07_D.c
@@ -1,20 +1,21 @@
-﻿#include<stdio.h>
+﻿//行列の積を表示するプログラム
+#include<stdio.h>
 
 int main(void) {
 	int n, m, l;
-	int A[100][100] = { {0} }, B[100][100] = { {0} };
-	int C[100][100] = { {0} };
+	long int A[100][100], B[100][100];  //行列A,Bの宣言
+	long int C[100][100] = { {0} };  //A,Bの積に用いる行列の宣言
 	int i, j, k;
 
-	scanf("%d %d", &n, &m, &l);  //列、行数の入力
+	scanf("%ld %ld %ld", &n, &m, &l);  //列、行数の入力
 	for (i = 0;i < n;i++) {
 		for (j = 0;j < m;j++) {
-			scanf("%d", &A[i][j]);  //行列Aの入力
+			scanf("%ld", &A[i][j]);  //行列Aの入力
 		}
 	}
 	for (i = 0;i < m;i++) {
 		for (j = 0;j < l;j++) {
-			scanf("%d", &B[i][j]);	
+			scanf("%ld", &B[i][j]);	  //行列Bの入力
 		}
 	}
 	//行列の積Cの計算
@@ -26,9 +27,11 @@ int main(void) {
 		}
 	}
 	for (i = 0;i < n;i++) {
-		for (j = 0;j < l;j++) {
-			printf("%d", C[i][j]);  //Cの表示
+		printf("%ld", C[i][0]);
+		for (j = 1;j < l;j++) {
+			printf(" %ld", C[i][j]);  //Cの表示
 		}
+		printf("\n");
 	}
 
 	return 0;
