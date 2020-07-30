4f566bcaec50df5dd798a773db1cc38b2fb8a7f2
OF222 <0214osuke@gmail.com>
2020-07-30 11:53:53 +0900

    課題の作成

--- ITP1_07_A.c
+++ ITP1_07_A.c
@@ -1,1 +1,31 @@
-﻿
+﻿//成績をつけるプログラム
+#include<stdio.h>
+
+int main(viod) {
+	int mid, fin, re;  //中間試験、期末試験、再試験の点数入力用の変数
+
+	scanf("%d %d %d", &mid, &fin, &re);  //各値の入力
+	while ((mid != -1) || (fin != -1) || (re != -1)) {  //すべての値に-1が入力されるまで繰り返す
+		if ((mid == -1) || (fin == -1)) {
+			printf("F\n");
+		}
+		else if (mid + fin < 30) {
+			printf("F\n");
+		}
+		else if ((mid + fin >= 30) && (mid + fin < 50)&&(re < 50)) {
+			printf("D\n");
+		}
+		else if (((mid + fin >= 50) && (mid + fin < 65)) || ((mid + fin >= 30) && (mid + fin < 50) && (re >= 50))){
+			printf("C\n");
+		}
+		else if ((mid + fin >= 65) && (mid + fin < 80)) {
+			printf("B\n");
+		}
+		else if(mid + fin >= 80){
+			printf("A\n");
+		}
+		scanf("%d %d %d", &mid, &fin, &re);  //各値の入力
+	}
+
+	return 0;
+}
--- ITP1_07_C.c
+++ ITP1_07_C.c
@@ -1,1 +1,34 @@
-﻿
+﻿//与えられた表の行、列の各合計を加えて再度表示するプログラム
+#include<stdio.h>
+
+int main(void) {
+	int table[100][100] = { {0} };
+	int m, n, i, j;
+
+	scanf("%d %d", &m, &n);
+	for (i = 0;i < m;i++) {
+		for (j = 0;j < n;j++) {
+			scanf("%d", &table[i][j]);
+		}
+	}
+	for (j = 0;j < n;j++) {
+		for (i = 0;i < m;i++) {
+			table[m][j] = table[m][j] + table[i][j];
+		}
+	}
+	for (i = 0;i < m + 1;i++) {
+		for (j = 0;j < n;j++) {
+			table[i][n] = table[i][n] + table[i][j];
+		}
+	}
+
+	for (i = 0;i < m + 1;i++) {
+		for (j = 0;j < n;j++) {
+			printf("%d ", table[i][j]);
+		}
+		printf("%d", table[i][j]);
+		printf("\n");
+	}
+	
+	return 0;
+}
